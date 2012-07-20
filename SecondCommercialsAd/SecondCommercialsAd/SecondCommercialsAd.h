b9a88653c28569a5d541e90fc6dc7fa7df09a6ab
tntlym <tntlym@gmail.com>
2012-07-20 14:06:17 +0800

    [scm] FE code initial commit - version 3_0_2

---
+++ SecondCommercialsAd/SecondCommercialsAd/SecondCommercialsAd.h
@@ -0,0 +1,56 @@
+//
+//  SecondCommercialsAd.h
+//  SecondCommercialsAd
+//
+//  Created by Yongmo Liang on 11/4/11.
+//  Copyright (c) 2011 SecondCommercials. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SecondCommercialsAd/ASIHTTPRequest.h>
+#import <SecondCommercialsAd/ASIFormDataRequest.h>
+#import <SecondCommercialsAd/FBConnect.h>
+#import <SecondCommercialsAd/SA_OAuthTwitterEngine.h>
+#import <SecondCommercialsAd/SA_OAuthTwitterController.h>
+
+
+typedef enum BannerPosition {
+    TOP = 0,
+    BOTTOM
+}BannerPosition;
+
+@protocol SecondCommercialsAdDelegate <NSObject>
+- (void)scmAdViewWillShow;
+- (void)scmAdViewDidFinish;
+- (void)scmAdBannerWillShow;
+@end
+
+@interface SecondCommercialsAd : UIViewController <NSXMLParserDelegate, UIAlertViewDelegate, FBSessionDelegate_Scm, FBRequestDelegate_Scm, ASIHTTPRequestDelegate, SA_OAuthTwitterEngineDelegate, SA_OAuthTwitterControllerDelegate> {
+    
+    // scmAdView - A view container it contains a banner and stamp views
+    UIView *scmAdView;
+
+    
+    // delegate
+    id <SecondCommercialsAdDelegate> scmAdDelegate;
+    
+   
+    
+    /************************************************************/
+    
+}
+
+@property (nonatomic, retain) UIView *scmAdView;
+@property (nonatomic, assign) id <SecondCommercialsAdDelegate> scmAdDelegate;
+
+
+
+
+- (id)initWithPosition:(BannerPosition)position;
+- (void) scmShowAdBannerView : (NSInteger)point;
+- (void) scmHideAdBannerView;
+
+- (void) scmClearScmAd;
+
+
+@end
