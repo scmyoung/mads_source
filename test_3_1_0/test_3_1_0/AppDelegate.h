20caf4824166257e108ce968eefc8867aa31d19e
Yongmo <tntlym@gmail.com>
2012-09-19 21:02:33 +0800

    add test_3_1_0

---
+++ test_3_1_0/test_3_1_0/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test_3_1_0
+//
+//  Created by Yongmo on 8/16/12.
+//  Copyright (c) 2012 Yongmo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
