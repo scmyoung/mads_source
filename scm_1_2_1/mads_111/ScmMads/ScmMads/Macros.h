8b242f058354cf09912b9cbffa62dfd43e7ade1e
Yongmo <tntlym@gmail.com>
2012-12-17 15:28:05 +0800

    init 1_2_1

---
+++ scm_1_2_1/mads_111/ScmMads/ScmMads/Macros.h
@@ -0,0 +1,78 @@
+//
+//  Macros.h
+//  ScmMads
+//
+//  Created by Yongmo on 8/29/12.
+//  Copyright (c) 2012 SecondCommercials. All rights reserved.
+//
+
+#ifndef ScmMads_Macros_h
+#define ScmMads_Macros_h
+
+#define SERVER_IP               @"http://211.115.71.69"
+#define LOCAL_SERVER_IP         @"http://localhost/"
+
+// **** Images ****
+
+/* -- OLD --
+
+#define IMG_SNS_CONNECT_P       @"connect_portrait.png"
+#define IMG_STAMP_P             @"stamp_portrait.png"
+#define IMG_MISSED_P            @"missed_portrait.png"
+#define IMG_DEFAULT_P           @"scmdefault_portrait.png"
+
+#define IMG_SNS_CONNECT_L       @"connect_landscape.png"
+#define IMG_STAMP_L             @"stamp_landscape.png"
+#define IMG_MISSED_L            @"missed_landscape.png"
+#define IMG_DEFAULT_L           @"scmdefault_landscape.png"
+
+#define IMG_X_MARK              @"xmark.png"
+#define IMG_ARROW               @"arrow.png"
+
+*/
+ 
+#define IMG_BANNER_P                @"portrait_banner.png"
+#define IMG_BANNER_MISSED_P         @"portrait_banner_missed.png"
+#define IMG_STAMP_P                 @"portrait_stamp.png"
+#define IMG_MISSED_P                @"portrait_stamp_missed.png"
+
+#define IMG_BANNER_L                @"landscape_banner.png"
+#define IMG_BANNER_MISSED_L         @"landscape_banner_missed.png"
+#define IMG_STAMP_L                 @"landscape_stamp.png"
+#define IMG_MISSED_L                @"landscape_stamp_missed.png"
+
+#define IMG_DEFAULT_P           @"portrait_default_stamp.png"
+#define IMG_DEFAULT_L           @"landscape_default_stamp.png"
+
+#define IMG_DEFAULT_BANNER_P    @"portrait_default_banner.png"
+#define IMG_DEFAULT_BANNER_L    @"landscape_default_banner.png"
+
+
+#define IMG_X_MARK                  @"xmark.png"
+
+#define PASSBOOK_PKG                @"coupon.pkpass"
+#define IMG_PB_BADGE                @"pkbadge.png"
+#define IMG_Q10_BADGE               @"q10badge.png"
+
+#define IMG_CONNECTED_FB_A                    @"connected_a_FB.png"
+#define IMG_CONNECTED_FB_B                    @"connected_b_FB.png"
+#define IMG_CONNECTED_TW_A                    @"connected_a_TWT.png"
+#define IMG_CONNECTED_TW_B                    @"connected_b_TWT.png"
+
+#define IMG_GET_FB                            @"get_FB.png"
+#define IMG_GET_TW                            @"get_TWT.png"
+
+
+
+
+// **** End of Images ****
+
+#define SCM_AD_XML              @"scmAdInfo.xml"
+#define SCM_AD_PLIST            @"scmAdPlist.plist"
+#define SCM_SNS_PLIST            @"scmSnsPlist.plist"
+
+#define FB_APP_ID               @"196736437067322"
+
+#define PHP_LOGIC_FILE          @"mads_3_1_0.php"
+
+#endif
---
+++ scm_1_2_1/mads_111/ScmMads/ScmMads/macro.h
@@ -0,0 +1,17 @@
+//
+//  macro.h
+//  ScmMads
+//
+//  Created by Yongmo on 8/17/12.
+//  Copyright (c) 2012 SecondCommercials. All rights reserved.
+//
+
+#ifndef ScmMads_macro_h
+#define ScmMads_macro_h
+
+#define SERVER_IP           @"211.115.71.69"
+#define LOCAL_SERVER_IP     @"http://localhost/"
+
+
+
+#endif
